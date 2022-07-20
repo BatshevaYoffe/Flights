07a95ccee1aeeb78f3396762d1705078d6302bac
Naamafb <naamafb12@gmail.com>
2022-07-20 18:29:10 +0300

    change the place of conectiong to the data base

---
+++ DAL/ConectionToDB.cs
@@ -0,0 +1,31 @@
+﻿using FlightModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ConectionToDB
+    {
+        void addFlight(FlightInfoPartial flight)
+        {
+            using (var ctx = new FlightContext())
+            {
+                ctx.Flights.Add(flight);
+                ctx.SaveChanges();
+            }
+        }
+    }
+    public class FlightContext : DbContext
+    {
+        public FlightContext() : base("FlightsDB")
+        {
+
+        }
+
+        public DbSet<FlightInfoPartial> Flights { get; set; }
+
+    }
+}
