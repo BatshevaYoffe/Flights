de4112d746c59ae3ef39e4e77226c26c41cbf072
Naamafb <naamafb12@gmail.com>
2022-08-05 11:22:23 +0300

    add information to conectionToDB

--- DAL/ConectionToDB.cs
+++ DAL/ConectionToDB.cs
@@ -10,14 +10,26 @@ namespace DAL
 {
     public class ConectionToDB
     {
-        void addFlight(FlightInfoPartial flight)
+        void addFlight(FlightInfoPartial flightIP)
         {
             using (var ctx = new FlightContext())
             {
+                var flight = new Flight() { FlightCode = flightIP.FlightCode, Source = flightIP.Source, Destination = flightIP.Destination, DateTime = flightIP.DateTime };
                 ctx.Flights.Add(flight);
                 ctx.SaveChanges();
             }
         }
+        //function to get flights that selected by date
+        List<Flight> returnFlightByDate(DateTime dateTime)
+        { 
+            using (var ctx = new FlightContext())
+            {
+                var flights = (from f in ctx.Flights where f.DateTime==dateTime select f ).ToList<Flight>();
+                return flights;
+            }
+        }
+
+
     }
     public class FlightContext : DbContext
     {
@@ -26,7 +38,14 @@ namespace DAL
 
         }
 
-        public DbSet<FlightInfoPartial> Flights { get; set; }
+        public DbSet<Flight> Flights { get; set; }
 
+    }
+    public class Flight
+    {
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public string FlightCode { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
