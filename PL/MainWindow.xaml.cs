d7867431c8b35b25b3d9d26097be18742504e9c1
BatshevaYoffe <73179986+BatshevaYoffe@users.noreply.github.com>
2022-08-05 14:38:20 +0300

    merge

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace PL
 
          VM.ViewModel vm =new VM.ViewModel();
 
-        public MainWindow()
+        public MainWindow()//
         {
             InitializeComponent();
         }
